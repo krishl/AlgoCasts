6659238814cb3dc8184a65deb876e7782ff18d01
github username <krishel.lasam@gmail.com>
2018-08-12 12:18:03 -0400

    complete maxchar exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let charCount = {}
+    let highestValue = 0
+    let maxChar = ''
+
+    for (let char of str) {
+        charCount[char] = charCount[char]++ || 1
+    }
+    for (let char in charCount) {
+        if (charCount[char] > highestValue) {
+            highestValue = charCount[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
 
 module.exports = maxChar;
