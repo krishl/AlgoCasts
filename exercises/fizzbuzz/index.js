94c40ec40e1c0b04e147439cf43e586e175b0927
github username <krishel.lasam@gmail.com>
2018-08-13 23:25:13 -0400

    complete fizzbuzz exercise

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,18 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let currentNumber = 1; currentNumber <= n; currentNumber++) {
+        if ((currentNumber % 3 === 0) && (currentNumber % 5 === 0)) {
+            console.log("fizzbuzz")
+        } else if (currentNumber % 3 === 0) {
+            console.log("fizz")
+        } else if (currentNumber % 5 === 0) {
+            console.log("buzz")
+        } else {
+            console.log(currentNumber)
+        }
+    }
+}
 
 module.exports = fizzBuzz;
